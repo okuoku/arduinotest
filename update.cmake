cff720af283469124547c7993adfbf71d71f53cf
okuoku <mjt@cltn.org>
2021-11-03 04:03:21 +0900

    Adjust path handling

--- update.cmake
+++ update.cmake
@@ -1,20 +1,30 @@
 execute_process(
     COMMAND ${CMAKE_COMMAND}
-    -DROOT=${CMAKE_CURRENT_LIST_DIR}/ArduinoCore-samd
-    -DPATH=cores/arduino
+    -DROOT=${CMAKE_CURRENT_LIST_DIR}
+    -DPATH=ArduinoCore-samd/cores/arduino
     -DTYPE=core
     -P ${CMAKE_CURRENT_LIST_DIR}/gen_arduino_srctab.cmake
     OUTPUT_FILE core.cmake
     ERROR_FILE core.cmake
     )
-
 execute_process(
     COMMAND ${CMAKE_COMMAND}
     -DREPO=ArduinoCore-samd
-    -DROOT=${CMAKE_CURRENT_LIST_DIR}/ArduinoCore-samd
-    -DPATH=libraries
+    -DROOT=${CMAKE_CURRENT_LIST_DIR}
+    -DPATH=ArduinoCore-samd/libraries
     -DTYPE=lib
     -P ${CMAKE_CURRENT_LIST_DIR}/gen_arduino_srctab.cmake
     OUTPUT_FILE libs.cmake
     ERROR_FILE libs.cmake
+    )
+
+execute_process(
+    COMMAND ${CMAKE_COMMAND}
+    -DREPO=seeedlibs
+    -DROOT=${CMAKE_CURRENT_LIST_DIR}
+    -DPATH=seeedlibs
+    -DTYPE=lib
+    -P ${CMAKE_CURRENT_LIST_DIR}/gen_arduino_srctab.cmake
+    OUTPUT_FILE libs_seeed.cmake
+    ERROR_FILE libs_seeed.cmake
     )
